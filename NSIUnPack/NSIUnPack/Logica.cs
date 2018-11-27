10b251789d95b1997c4e170ffc790636cd590a6b
Egor Timashev <metallagetemp@gmail.com>
2018-11-27 16:48:37 +0300

    Первые наброски

---
+++ NSIUnPack/NSIUnPack/Logica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NSIUnPack
+{
+    class Logica
+    {
+        private string tempPath = @"c:\temp\nsi\";
+        private string inPath = @"C:\temp\inNSI";
+        private string outPath = @"c:\Temp\outNSI";
+    }
+}
---
+++ NSIUnPack/NSIUnPack/archNSI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NSIUnPack
+{
+    class archNSI
+    {
+        private string filePath;
+
+        public archNSI(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public bool UnZipNSI(string outputPath)
+        {
+            bool isSuccess = false;
+
+
+            return isSuccess;
+        }
+
+        public bool ExterminateMe()
+        {
+            bool exterminated = false;
+
+
+            return exterminated;
+        }
+
+
+    }
+}
