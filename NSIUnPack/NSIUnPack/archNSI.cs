b6a4dae19cf9b244592312751a1365bf537293dc
Egor Timashev <metallagetemp@gmail.com>
2018-12-06 17:39:51 +0300

    Добавил функционал распаковки

--- NSIUnPack/NSIUnPack/archNSI.cs
+++ NSIUnPack/NSIUnPack/archNSI.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
 
 namespace NSIUnPack
 {
@@ -29,7 +32,84 @@ namespace NSIUnPack
 
             return exterminated;
         }
+        /// <summary>
+        /// Нашел эту функцию в гугле, нужна для извлечения файлов с помощью 7zip
+        /// </summary>
+        /// <param name="archiverEXE">полный путь к 7zip.exe</param>
+        /// <param name="archiveFile">Файл архива</param>
+        /// <param name="outputFolder">Дирректория назначения распаковки</param>
+        /// <returns></returns>
+        private bool ExtractME(string archiverEXE, string archiveFile, string outputFolder) 
+        {
+            bool isSucsess = false;
+            try
+            {
+                // Предварительные проверки
+                if (!File.Exists(archiverEXE))
+                    throw new Exception("Архиватор 7z по пути \"" + archiverEXE +
+                    "\" не найден");
+                if (!File.Exists(archiveFile))
+                    throw new Exception("Файл архива \"" + archiveFile +
+                    "\" не найден");
+                if (!Directory.Exists(outputFolder))
+                    Directory.CreateDirectory(outputFolder);
 
+                // Формируем параметры вызова 7z
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = archiverEXE;
+                // Распаковать (для полных путей - x)
+                startInfo.Arguments = " e";
+                // На все отвечать yes
+                startInfo.Arguments += " -y";
+                // Файл, который нужно распаковать
+                startInfo.Arguments += " " + "\"" + archiveFile + "\"";
+                // Папка распаковки
+                startInfo.Arguments += " -o" + "\"" + outputFolder + "\"";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                int sevenZipExitCode = 0;
+                using (Process sevenZip = Process.Start(startInfo))
+                {
+                    sevenZip.WaitForExit();
+                    sevenZipExitCode = sevenZip.ExitCode;
+                }
+                // Если с первого раза не получилось,
+                //пробуем еще раз через 1 секунду
+
+                if (sevenZipExitCode == 0 || sevenZipExitCode == 1)
+                {
+                    isSucsess = true;
+                }
+                else
+                {
+                    using (Process sevenZip = Process.Start(startInfo))
+                    {
+                        Thread.Sleep(1000);
+                        sevenZip.WaitForExit();
+                        switch (sevenZip.ExitCode)
+                        {
+                            case 0: isSucsess = true;
+                                break; // Без ошибок и предупреждений
+                            case 1: isSucsess = true;
+                                break; // Есть некритичные предупреждения
+                            case 2: throw new Exception("Фатальная ошибка");
+                            case 7: throw new Exception("Ошибка в командной строке");
+                            case 8:
+                                throw new Exception("Недостаточно памяти для выполнения операции");
+                            case 225:
+                                throw new Exception("Пользователь отменил выполнение операции");
+                            default:
+                                throw new Exception("Архиватор 7z вернул недокументированный код ошибки: " 
+                                    + sevenZip.ExitCode.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("SevenZip.ExtractFromArchive: " + e.Message);
+            }
+            return isSucsess;
+        }
 
     }
 }
